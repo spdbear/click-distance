a012725d5362c7fd4f18dc7625a078ca3b37befc
spdbear <16793732+spdbear@users.noreply.github.com>
2021-04-20 03:34:54 +0900

    画像クリックで座標を出すところまで

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,32 @@
 <script lang="ts">
+  import { trackData } from './data'
+  const nameList = trackData.map((v) => v.abbr)
+  let selected = 1
+  $: imagePath = `assets/atbl_${('0' + selected).slice(-2)}.png`
+	$: cor = {x: 0, y: 0}
+  const toggleImage = () => {}
+	const clickImage = (e: MouseEvent) => {
+		cor.x = e.offsetX
+		cor.y = e.offsetY
+	}
 </script>
 
 <main>
+  <select bind:value={selected}>
+    {#each trackData as track}
+      <option value={track.id}>
+        {track.name_en}
+      </option>
+    {/each}
+  </select>
   <div class="canvas-wrapper">
-    <div class="image" />
-    <div class="image2" />
-    <canvas class="canvas" />
+    <div class="image" style="background-image: url('{imagePath}');"  on:click={clickImage} />
   </div>
   <div class="button-wrapper">
-    <button id="show">Show Answer</button>
+    <button class="toggle" on:click={toggleImage}>Toggle Image</button>
     <button id="clear">Clear Canvas</button>
   </div>
-  <div class="text" />
+  <div class="text">{cor.x},{cor.y}</div>
 </main>
 
 <style>
@@ -22,20 +37,14 @@
     margin: 0 auto;
   }
 
-  .image,
-  .image2 {
+  .image {
     width: 350px;
     height: 350px;
-    margin-right: 10px;
+    margin: 0 auto;
     background-size: 350px;
   }
-
   .image {
     background-image: url('https://i.imgur.com/DQeyuzY.png');
-  }
-  .image2 {
-    background-image: url('https://i.imgur.com/6d22VN2.png');
-    opacity: 0;
   }
 
   @media (min-width: 640px) {
