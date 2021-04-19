b3945a98061e92d77a5595c4906d79831e1a1db9
spdbear <16793732+spdbear@users.noreply.github.com>
2021-04-19 23:58:57 +0900

    CodePenの雛形をマークアップだけ移植

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,46 @@
 <script lang="ts">
-	export let name: string;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <div class="canvas-wrapper">
+    <div class="image" />
+    <div class="image2" />
+    <canvas class="canvas" />
+  </div>
+  <div class="button-wrapper">
+    <button id="show">Show Answer</button>
+    <button id="clear">Clear Canvas</button>
+  </div>
+  <div class="text" />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 700px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  .image,
+  .image2 {
+    width: 350px;
+    height: 350px;
+    margin-right: 10px;
+    background-size: 350px;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  .image {
+    background-image: url('https://i.imgur.com/DQeyuzY.png');
+  }
+  .image2 {
+    background-image: url('https://i.imgur.com/6d22VN2.png');
+    opacity: 0;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
